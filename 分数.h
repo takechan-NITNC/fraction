7e95fa812103616291c60f498ada8be500c5c7d5
たけちゃん <79554611+takechan-NITNC@users.noreply.github.com>
2023-02-02 13:58:19 +0900

    ヘッダファイルから仮引数名を削除

--- 分数.h
+++ 分数.h
@@ -3,7 +3,7 @@ struct fraction {
 	int denominator;
 	int numerator;
 };
-struct fraction fraction_sum(struct fraction fraction1, struct fraction fraction2);
-struct fraction fraction_substract(struct fraction substracted, struct fraction substracting);
-struct fraction fraction_product(struct fraction fraction1, struct fraction fraction2);
-struct fraction fraction_divide(struct fraction divided, struct fraction dividing);
+struct fraction fraction_sum(struct fraction, struct fraction);
+struct fraction fraction_substract(struct fraction, struct fraction);
+struct fraction fraction_product(struct fraction, struct fraction);
+struct fraction fraction_divide(struct fraction, struct fraction);
